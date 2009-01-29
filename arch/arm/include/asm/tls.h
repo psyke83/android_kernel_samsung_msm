02a77cef2e064abd518f9977a3c0fbbe76fb2f1e
Brian Swetland <swetland@google.com>
2009-01-28 20:45:48 -0800

    HACK stay compatible with the emulated TLS register HACK
    psyke83: wrap hack into CONFIG_TLS_USERSPACE_EMUL option
    
    Change-Id: Ia59b8f4422cb56a9954f06d7ac06338bbfe71e59
    Signed-off-by: Pankaj <pankaj.bharadiya@ti.com>

--- arch/arm/include/asm/tls.h
+++ arch/arm/include/asm/tls.h
@@ -7,8 +7,13 @@
 
 	.macro set_tls_v6k, tp, tmp1, tmp2
 	mcr	p15, 0, \tp, c13, c0, 3		@ set TLS register
+#ifdef CONFIG_TLS_USERSPACE_EMUL
+	mov	\tmp1, #0xffff0fff
+	str	\tp, [\tmp1, #-15]		@ set TLS value at 0xffff0ff0
+#else
 	mov	\tmp1, #0
 	mcr	p15, 0, \tmp1, c13, c0, 2	@ clear user r/w TLS register
+#endif
 	.endm
 
 	.macro set_tls_v6, tp, tmp1, tmp2
@@ -17,9 +22,13 @@
 	mov	\tmp2, #0xffff0fff
 	tst	\tmp1, #HWCAP_TLS		@ hardware TLS available?
 	mcrne	p15, 0, \tp, c13, c0, 3		@ yes, set TLS register
+#ifndef CONFIG_TLS_USERSPACE_EMUL
 	movne	\tmp1, #0
 	mcrne	p15, 0, \tmp1, c13, c0, 2	@ clear user r/w TLS register
 	streq	\tp, [\tmp2, #-15]		@ set TLS value at 0xffff0ff0
+#else
+	str	\tp, [\tmp2, #-15]		@ set TLS value at 0xffff0ff0
+#endif
 	.endm
 
 	.macro set_tls_software, tp, tmp1, tmp2
